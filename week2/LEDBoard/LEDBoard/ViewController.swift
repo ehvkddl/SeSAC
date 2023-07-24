6fb67eeb1c38e91a70e57f2c15764f5a8b82f0b7
ehvkddl <ehvkddl@naver.com>
2023-07-25 01:31:11 +0900

    design: Outlet 연결

--- week2/LEDBoard/LEDBoard/ViewController.swift
+++ week2/LEDBoard/LEDBoard/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var returnButton: UIButton!
+    
+    @IBOutlet var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
