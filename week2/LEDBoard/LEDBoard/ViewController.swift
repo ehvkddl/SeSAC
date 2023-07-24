605c1b5b21c9df45777e6f481f53e8403f859c4a
ehvkddl <ehvkddl@naver.com>
2023-07-25 00:16:37 +0900

    init: LEDBoard 생성

---
+++ week2/LEDBoard/LEDBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LEDBoard
+//
+//  Created by do hee kim on 2023/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
