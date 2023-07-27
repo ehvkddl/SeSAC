e61d0de9aaf538a01251986dea238a6c24f5ce44
ehvkddl <ehvkddl@naver.com>
2023-07-27 17:15:25 +0900

    add: Setting enum 생성

---
+++ week2/SeSACWeek3/SeSACWeek3/Assignment/Setting.swift
@@ -0,0 +1,39 @@
+//
+//  Setting.swift
+//  SeSACWeek3
+//
+//  Created by do hee kim on 2023/07/27.
+//
+
+import Foundation
+
+enum Setting: String {
+    case wholeSetting = "전체 설정"
+    case personalSetting = "개인 설정"
+    case etc = "기타"
+    
+    var text: String { rawValue }
+    
+    enum WholeSetting: String {
+        case notice = "공지사항"
+        case lab = "실험실"
+        case version = "버전 정보"
+        
+        var text: String { rawValue }
+    }
+    
+    enum PeronalSetting: String {
+        case personalSecurity = "개인/보안"
+        case notification = "알림"
+        case chatting = "채팅"
+        case multiprofile = "멀티프로필"
+        
+        var text: String { rawValue }
+    }
+    
+    enum Etc: String {
+        case help = "고객센터/도움말"
+        
+        var text: String { rawValue }
+    }
+}
