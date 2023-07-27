7997b86f7d093480cccd446af4fa17aa90d297fd
ehvkddl <ehvkddl@naver.com>
2023-07-28 01:07:51 +0900

    feat: add button 클릭 시 todo 추가

--- week2/SeSACWeek3/SeSACWeek3/Assignment/Case3TableViewController.swift
+++ week2/SeSACWeek3/SeSACWeek3/Assignment/Case3TableViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class Case3TableViewController: UITableViewController {
 
+    var todos: [Todo] = []
+    
+    @IBOutlet var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    @IBAction func addBtnClicked(_ sender: UIButton) {
+        view.endEditing(true)
+        
+        guard let todo = textField.text else { return }
+        
+        todos.append(Todo(todo: todo,
+                          isDone: false,
+                          isFavorite: false))
+        
+        textField.text = ""
+    }
+    
 
 }
