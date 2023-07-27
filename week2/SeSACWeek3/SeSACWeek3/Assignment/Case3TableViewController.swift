e008bb17fb972ec8dedd1689477abf80b44672e3
ehvkddl <ehvkddl@naver.com>
2023-07-28 00:46:38 +0900

    init: Case 3 생성

---
+++ week2/SeSACWeek3/SeSACWeek3/Assignment/Case3TableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  Case3TableViewController.swift
+//  SeSACWeek3
+//
+//  Created by do hee kim on 2023/07/28.
+//
+
+import UIKit
+
+class Case3TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
