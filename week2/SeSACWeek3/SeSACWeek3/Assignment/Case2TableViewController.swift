053f0543d75a2e502f738d3a910ee225c0dcf8a4
ehvkddl <ehvkddl@naver.com>
2023-07-27 17:13:21 +0900

    init: Case 2 생성

---
+++ week2/SeSACWeek3/SeSACWeek3/Assignment/Case2TableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  Case2TableViewController.swift
+//  SeSACWeek3
+//
+//  Created by do hee kim on 2023/07/27.
+//
+
+import UIKit
+
+class Case2TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        <#code#>
+    }
+
+}
