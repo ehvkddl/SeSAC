a7198d1c2d3185d49e0815c00a6a931ef7df00bf
ehvkddl <ehvkddl@naver.com>
2023-07-29 17:57:23 +0900

    init: Movie 생성

---
+++ week2/SeSACWeek3/SeSACWeek3/Assignment/Movie/MovieTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieTableViewCell.swift
+//  SeSACWeek3
+//
+//  Created by do hee kim on 2023/07/29.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+}
---
+++ week2/SeSACWeek3/SeSACWeek3/Assignment/Movie/MovieTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MovieTableViewController.swift
+//  SeSACWeek3
+//
+//  Created by do hee kim on 2023/07/29.
+//
+
+import UIKit
+
+class MovieTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
