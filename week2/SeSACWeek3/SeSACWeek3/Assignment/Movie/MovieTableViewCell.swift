4d7a5e04c1ada3fc9209961207348aa98bac77e3
ehvkddl <ehvkddl@naver.com>
2023-07-29 19:12:40 +0900

    design: cell design

--- week2/SeSACWeek3/SeSACWeek3/Assignment/Movie/MovieTableViewCell.swift
+++ week2/SeSACWeek3/SeSACWeek3/Assignment/Movie/MovieTableViewCell.swift
@@ -21,6 +21,19 @@ class MovieTableViewCell: UITableViewCell {
     
     @IBOutlet var overviewLabel: UILabel!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        titleLabel.font = UIFont.systemFont(ofSize: 17, weight: .heavy)
+        
+        releaseDateLabel.font = UIFont.preferredFont(forTextStyle: .callout)
+        runtimeLabel.font = UIFont.preferredFont(forTextStyle: .callout)
+        rateLabel.font = UIFont.preferredFont(forTextStyle: .callout)
+        
+        overviewLabel.numberOfLines = 0
+        overviewLabel.font = UIFont.preferredFont(forTextStyle: .footnote)
+    }
+    
     func configureCell(row: Movie) {
         posterImageView.image = UIImage(named: row.title)
         
