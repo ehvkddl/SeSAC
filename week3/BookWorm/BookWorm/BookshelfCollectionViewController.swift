a943f7229e1079b08127b8d487c4b0800edec6be
ehvkddl <ehvkddl@naver.com>
2023-07-31 17:56:05 +0900

    feat: collectionView 크기 지정

--- week3/BookWorm/BookWorm/BookshelfCollectionViewController.swift
+++ week3/BookWorm/BookWorm/BookshelfCollectionViewController.swift
@@ -16,8 +16,21 @@ class BookshelfCollectionViewController: UICollectionViewController {
 
         let nib = UINib(nibName: "BookshelfCollectionViewCell", bundle: nil)
         collectionView.register(nib, forCellWithReuseIdentifier: "BookshelfCollectionViewCell")
+        
+        searchCollectionViewLayout()
     }
+    
+    func searchCollectionViewLayout() {
+        let layout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 10
+        let width = UIScreen.main.bounds.width - (spacing * 3)
 
+        layout.itemSize = CGSize(width: width / 2, height: width / 2 * 2.0)
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        collectionView.collectionViewLayout = layout
+    }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return movieInfo.movie.count
