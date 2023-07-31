fd16f90f5184f26038e9302b72973f5f8ab4438b
ehvkddl <ehvkddl@naver.com>
2023-07-31 17:04:44 +0900

    add: BookshelfCollectionViewCell 생성

---
+++ week3/BookWorm/BookWorm/BookshelfCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  BookshelfCollectionViewCell.swift
+//  BookWorm
+//
+//  Created by do hee kim on 2023/07/31.
+//
+
+import UIKit
+
+class BookshelfCollectionViewCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
