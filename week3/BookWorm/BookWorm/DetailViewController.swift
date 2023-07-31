5433ee522c5e7a6c31bd6f075285cab61358e7f2
ehvkddl <ehvkddl@naver.com>
2023-08-01 01:38:04 +0900

    feat: detailView navigation bar item 수정

--- week3/BookWorm/BookWorm/DetailViewController.swift
+++ week3/BookWorm/BookWorm/DetailViewController.swift
@@ -14,9 +14,20 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.hidesBackButton = true
+        
+        let back = UIImage(systemName: "chevron.backward")
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: back, style: .plain, target: self, action: #selector(backButtonClicked))
+        setBarButtonItemColor(color: .black)
+        
         guard let movie = contents else { return }
         title = movie.title
     }
     
+    @objc
+    func backButtonClicked() {
+        navigationController?.popViewController(animated: true)
+    }
 
 }
