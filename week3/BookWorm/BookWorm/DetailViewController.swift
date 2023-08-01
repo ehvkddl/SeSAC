1adc61863036f293e1724ed3c0630034af7c7c9a
ehvkddl <ehvkddl@naver.com>
2023-08-02 02:21:05 +0900

    feat: detailView

--- week3/BookWorm/BookWorm/DetailViewController.swift
+++ week3/BookWorm/BookWorm/DetailViewController.swift
@@ -11,6 +11,16 @@ class DetailViewController: UIViewController {
 
     var contents: Movie?
     
+    @IBOutlet var backgroundView: UIView!
+    
+    @IBOutlet var moviePosterImageView: UIImageView!
+    
+    @IBOutlet var movieTitleLabel: UILabel!
+    @IBOutlet var runtimeAndRateLabel: UILabel!
+    @IBOutlet var releaseDateLabel: UILabel!
+    
+    @IBOutlet var overviewLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,10 +29,38 @@ class DetailViewController: UIViewController {
         let back = UIImage(systemName: "chevron.backward")
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: back, style: .plain, target: self, action: #selector(backButtonClicked))
-        setBarButtonItemColor(color: .black)
+        setBarButtonItemColor(color: .white)
+        
+        designCell()
         
         guard let movie = contents else { return }
+        
         title = movie.title
+        configureCell(movie: movie)
+    }
+    
+    func configureCell(movie: Movie) {
+        moviePosterImageView.image = UIImage(named: movie.title)
+        
+        movieTitleLabel.text = movie.title
+        runtimeAndRateLabel.text = "\(movie.runtime)분 | ⭐️\(movie.rate)점"
+        releaseDateLabel.text = "\(movie.releaseDate) 개봉"
+        
+        overviewLabel.text = movie.overview
+    }
+    
+    func designCell() {
+        backgroundView.backgroundColor = .black
+        
+        movieTitleLabel.textColor = .white
+        movieTitleLabel.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+        
+        runtimeAndRateLabel.textColor = .white
+        
+        releaseDateLabel.textColor = .white
+        releaseDateLabel.font = UIFont.systemFont(ofSize: 14, weight: .light)
+        
+        overviewLabel.numberOfLines = 0
     }
     
     @objc
