88245d0fd94bb6fc06c44c9e0c055648e49f8008
ehvkddl <ehvkddl@naver.com>
2023-08-01 16:29:27 +0900

    23.08.01

--- week2/SeSACWeek3/SeSACWeek3/CustomTableViewController.swift
+++ week2/SeSACWeek3/SeSACWeek3/CustomTableViewController.swift
@@ -9,12 +9,22 @@ import UIKit
 
 class CustomTableViewController: UITableViewController {
 
-    var todo = ToDoInformation()
+    var todo = ToDoInformation() {
+        didSet { // 변수가 달라짐을 감지!
+            print("DidSet")
+            tableView.reloadData()
+        }
+    }
+    
+    @IBOutlet var searchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.rowHeight = 90
+        
+        searchBar.placeholder = "할 일을 입력해주세요"
+        searchBar.searchTextField.addTarget(self, action: #selector(searchBarReturnTapped), for: .editingDidEndOnExit)
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -28,13 +38,49 @@ class CustomTableViewController: UITableViewController {
         
         cell.configureCell(row: row)
         
+        cell.likeButton.tag = indexPath.row
+        cell.likeButton.addTarget(self, action: #selector(likeButtonClicked), for: .touchUpInside)
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         todo.list[indexPath.row].done.toggle()
         
-        tableView.reloadData()
+//        tableView.reloadData()
+    }
+    
+    // 삭제
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        // 제거 -> 갱신
+        todo.list.remove(at: indexPath.row)
+        
+//        tableView.reloadData()
     }
 
+    @objc
+    func likeButtonClicked(_ sender: UIButton) {
+        print("HI \(sender.tag)")
+        
+        todo.list[sender.tag].like.toggle()
+        
+//        tableView.reloadData()
+    }
+    
+    @objc
+    func searchBarReturnTapped() {
+        // ToDo 항목을
+        let data = ToDo(main: searchBar.text!, sub: "23.08.01", like: false, done: false)
+        // list에 추가
+        todo.list.insert(data, at: 0)
+        //UX
+        searchBar.text = ""
+        // 갱신
+//        tableView.reloadData()
+    }
+    
 }
