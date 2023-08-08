cded32d3a68bf5d2620a7c82fbb35207c487d28b
ehvkddl <ehvkddl@naver.com>
2023-08-08 21:42:07 +0900

    feat: storyboard UI

--- week4/Lotto/Lotto/ViewController.swift
+++ week4/Lotto/Lotto/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var lottoNums: [UILabel]!
+    @IBOutlet var bonusNum: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
