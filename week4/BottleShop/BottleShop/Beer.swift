63b9e59c94e167b4146c78556b7496e8744b9a9c
ehvkddl <ehvkddl@naver.com>
2023-08-09 01:08:50 +0900

    refactor: Beer 구조체 수정

--- week4/BottleShop/BottleShop/Beer.swift
+++ week4/BottleShop/BottleShop/Beer.swift
@@ -7,14 +7,24 @@
 
 import Foundation
 
-struct Beer {
+struct Beer: Codable {
     var name: String
     var tagline: String
     var description: String
     var imageURL: String
     var abv: Double
-    var ibu: Int
-    var srm: Int
+    var ibu: Double?
+    var srm: Double?
     var foodPairing: [String]
     var brewersTips: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name, tagline
+        case description
+        case imageURL = "image_url"
+        case abv, ibu
+        case srm
+        case foodPairing = "food_pairing"
+        case brewersTips = "brewers_tips"
+    }
 }
