0568663cd4c7eff09c33312562e647c7b5beeb02
ehvkddl <ehvkddl@naver.com>
2023-08-08 21:45:07 +0900

    design: code UI
    - 숫자별 공 색

--- week4/Lotto/Lotto/ViewController.swift
+++ week4/Lotto/Lotto/ViewController.swift
@@ -12,11 +12,51 @@ class ViewController: UIViewController {
     
     @IBOutlet var lottoNums: [UILabel]!
     @IBOutlet var bonusNum: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        designUI()
+        
     }
 
 
 }
 
+extension ViewController {
+    
+    func designUI() {
+        for lottoNum in lottoNums {
+            circleBackground(lbl: lottoNum)
+            designLabel(lbl: lottoNum)
+        }
+    }
+    
+    func designLabel(lbl: UILabel) {
+        lbl.textColor = .white
+        
+        lbl.textAlignment = .center
+        lbl.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        
+        lbl.shadowOffset = CGSize(width: 1, height: 1)
+        lbl.shadowColor = .gray
+    }
+    
+    func circleBackground(lbl: UILabel, color: UIColor = .gray) {
+        lbl.backgroundColor = color
+        lbl.layer.cornerRadius = lbl.frame.width / 2
+        lbl.layer.masksToBounds = true
+    }
+    
+    func setCircleColor(num: Int) -> UIColor {
+        switch num {
+        case 1...9: return UIColor.LTYellow
+        case 10...19: return UIColor.LTBlue
+        case 20...29: return UIColor.LTRed
+        case 30...39: return UIColor.LTGray
+        case 40...45: return UIColor.LTGreen
+        default: return .gray
+        }
+    }
+    
+}
