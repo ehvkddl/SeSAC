fa01926984a8ba16e56f2947121d24459798eade
ehvkddl <ehvkddl@naver.com>
2023-08-08 23:24:51 +0900

    add: Beer 구조체 생성

---
+++ week4/BottleShop/BottleShop/Beer.swift
@@ -0,0 +1,20 @@
+//
+//  Beer.swift
+//  BottleShop
+//
+//  Created by do hee kim on 2023/08/08.
+//
+
+import Foundation
+
+struct Beer {
+    var name: String
+    var tagline: String
+    var description: String
+    var imageURL: String
+    var abv: Double
+    var ibu: Int
+    var srm: Int
+    var foodPairing: [String]
+    var brewersTips: String
+}
