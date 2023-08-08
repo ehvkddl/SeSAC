82d0851d3f73075a8fff83c24d9d423dac071bf8
ehvkddl <ehvkddl@naver.com>
2023-08-08 21:38:05 +0900

    add: color assets 추가

---
+++ week4/Lotto/Lotto/Extention+UIColor.swift
@@ -0,0 +1,16 @@
+//
+//  Extention+UIColor.swift
+//  Lotto
+//
+//  Created by do hee kim on 2023/08/08.
+//
+
+import UIKit
+
+extension UIColor {
+    static let LTYellow = UIColor(named: "LTYellow")!
+    static let LTBlue = UIColor(named: "LTBlue")!
+    static let LTRed = UIColor(named: "LTRed")!
+    static let LTGray = UIColor(named: "LTGray")!
+    static let LTGreen = UIColor(named: "LTGreen")!
+}
