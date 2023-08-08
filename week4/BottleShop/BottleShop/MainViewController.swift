bb6070c17c0888186bae8ba2f5fe411a53eb1e1f
ehvkddl <ehvkddl@naver.com>
2023-08-09 01:09:31 +0900

    feat: punk API 사용하여 맥주 정보 가져오기

--- week4/BottleShop/BottleShop/MainViewController.swift
+++ week4/BottleShop/BottleShop/MainViewController.swift
@@ -6,13 +6,36 @@
 //
 
 import UIKit
+import Alamofire
 
 class MainViewController: UIViewController {
+    var beers: [Beer] = []
 
     @IBOutlet var beerCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        callRequest()
+    }
+    
+    func callRequest() {
+        let url = "https://api.punkapi.com/v2/beers"
+
+        AF.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let decoder = JSONDecoder()
+                do {
+                    let data = try JSONSerialization.data(withJSONObject: value, options: .prettyPrinted)
+                    self.beers = try decoder.decode([Beer].self, from: data)
+                } catch {
+                    print(error.localizedDescription, "ㅠㅠ")
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 
 }
