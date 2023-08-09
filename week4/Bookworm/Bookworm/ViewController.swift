d29b8d0d102f98a242bb248be61f5bb1b9c75385
ehvkddl <ehvkddl@naver.com>
2023-08-10 00:36:06 +0900

    feat: Kakao API 책 정보 가져오기

--- week4/Bookworm/Bookworm/ViewController.swift
+++ week4/Bookworm/Bookworm/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    var books: [Book] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        callRequest(query: "스위프트")
     }
 
+    func callRequest(query: String) {
+        guard let text = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
+        let url = "https://dapi.kakao.com/v3/search/book?query=\(text)"
+        let header: HTTPHeaders = ["Authorization": "KakaoAK \(APIKey.kakaoKey)"]
+        
+        AF.request(url, method: .get, headers: header).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                do {
+                    let data = try JSONSerialization.data(withJSONObject: value)
+                    let document = try JSONDecoder().decode(Document.self, from: data)
+
+                    self.books = document.documents
+                } catch {
+                    print(error.localizedDescription)
+                }
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 
 }
-
