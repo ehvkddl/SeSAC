b0bd07c5983053bfd224024eab7eda897f8b9958
ehvkddl <ehvkddl@naver.com>
2023-08-10 00:35:46 +0900

    add: Book 구조체 생성

---
+++ week4/Bookworm/Bookworm/Book.swift
@@ -0,0 +1,48 @@
+//
+//  Book.swift
+//  Bookworm
+//
+//  Created by do hee kim on 2023/08/09.
+//
+
+import Foundation
+
+struct Document: Codable {
+    var documents: [Book]
+    var meta: Meta
+}
+
+struct Book: Codable {
+    var authors: [String]
+    var contents: String
+    var datetime: String
+    var isbn: String
+    var price: Int
+    var publisher: String
+    var salePrice: Int
+    var status: String
+    var thumbnail: String
+    var title: String
+    var translators: [String]
+    var url: String
+    
+    enum CodingKeys: String, CodingKey {
+        case authors, contents, datetime, isbn, price, publisher
+        case salePrice = "sale_price"
+        case status, thumbnail, title, translators, url
+    }
+}
+
+struct Meta: Codable {
+    var isEnd: Bool
+    var pageableCount: Int
+    var totalCount: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case isEnd = "is_end"
+        case pageableCount = "pageable_count"
+        case totalCount = "total_count"
+    }
+}
+
+
