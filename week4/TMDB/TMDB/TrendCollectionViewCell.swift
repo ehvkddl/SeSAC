fbc8d8ca0d5a3a3575d98150df6add87c40ee2ce
ehvkddl <ehvkddl@naver.com>
2023-08-13 03:05:10 +0900

    design: trend collectionview cell

--- week4/TMDB/TMDB/TrendCollectionViewCell.swift
+++ week4/TMDB/TMDB/TrendCollectionViewCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 class TrendCollectionViewCell: UICollectionViewCell {
 
@@ -13,8 +14,10 @@ class TrendCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet var releaseDateLabel: UILabel!
     @IBOutlet var genreLabel: UILabel!
+    @IBOutlet var mediaTypeLabel: UILabel!
     
     @IBOutlet var backdropImageView: UIImageView!
+    @IBOutlet var voteAverageLabel: PaddingLabel!
     
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var overviewLabel: UILabel!
@@ -26,10 +29,56 @@ class TrendCollectionViewCell: UICollectionViewCell {
         // Initialization code
     }
     
-    func configureCell(row: Movie) {
+    func configureCell(row: Trend) {
+        configureImage()
+        configureButton()
+        configureShadow()
+        
+        releaseDateLabel.text = row.releaseDate
+        genreLabel.text = row.genreIDS.map { "#\(String($0))" }.joined(separator: " ")
+        mediaTypeLabel.text = row.mediaType.rawValue
+        
+        let url = URL(string: "https://image.tmdb.org/t/p/w500/\(row.backdropPath)")
+        backdropImageView.kf.setImage(with: url)
+        
+        voteAverageLabel.text = String(row.voteAverage)
+        
+        titleLabel.text = row.title != nil ? row.title : row.name
+        overviewLabel.text = row.overview
     }
     
-    func setShadow() {
+    func configureImage() {
+        backdropImageView.contentMode = .scaleAspectFill
+        
+        backdropImageView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        backdropImageView.layer.cornerRadius = 10
+    }
+    
+    func configureButton() {
+        var config = UIButton.Configuration.filled() // apple system button
+        config.title = "자세히 보기"
+        config.image = UIImage(systemName: "chevron.right")
+        
+        config.buttonSize = .mini
+        config.baseForegroundColor = .black
+        config.baseBackgroundColor = .clear
+        
+        config.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0)
+        config.imagePadding = UIScreen.main.bounds.width * 0.58
+        config.imagePlacement = .trailing
+        
+        config.cornerStyle = .capsule
+        
+        moreDetailButton.configuration = config
+    }
+    
+    func configureShadow() {
+        contentsView.layer.cornerRadius = 10
+        
+        contentsView.layer.shadowColor = UIColor.black.cgColor
+        contentsView.layer.shadowOpacity = 0.3
+        contentsView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        contentsView.layer.shadowRadius = 10
     }
     
 }
