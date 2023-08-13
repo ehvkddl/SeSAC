d0fd115887069060f9927ff6795ddb245bedb420
ehvkddl <ehvkddl@naver.com>
2023-08-13 22:51:51 +0900

    feat: fetchCredit 메서드

--- week4/TMDB/TMDB/TmdbAPIManager.swift
+++ week4/TMDB/TMDB/TmdbAPIManager.swift
@@ -35,4 +35,27 @@ class TmdbAPIManager {
         }
     }
     
+    func fetchCredit(movieID: Int, completionHandler: @escaping (Credit) -> Void) {
+        let url = "https://api.themoviedb.org/3/movie/\(movieID)/credits?language=en-US&api_key=\(APIKey.tmdbKey)"
+        let headers: HTTPHeaders = [
+            "accept": "application/json",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MmFhNTI3ZWZkYjIwZWI5OTlkY2VjMWJmYWE4NWM5NiIsInN1YiI6IjY0ZDEzNzBkZDhkMzI5MDExZTc0YmE0ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.b02Ozt3Zntfjrhbw1uoEN-VJtaSF9zPxTkAG9gBIV78"
+        ]
+        
+        AF.request(url, method: .get, headers: headers).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                do {
+                    let data = try JSONSerialization.data(withJSONObject: value)
+                    let decodedData = try JSONDecoder().decode(Credit.self, from: data)
+                    
+                    completionHandler(decodedData)
+                } catch {
+                    print("[decode fail]", error.localizedDescription)
+                }
+            case .failure(let error):
+                print("ERROR", error)
+            }
+        }
+    }
 }
