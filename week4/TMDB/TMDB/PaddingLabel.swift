6f9b4ba0ba09161f4422399145c426f2b773cf64
ehvkddl <ehvkddl@naver.com>
2023-08-13 22:46:21 +0900

    feat: paddingLabel custom 수정

--- week4/TMDB/TMDB/PaddingLabel.swift
+++ week4/TMDB/TMDB/PaddingLabel.swift
@@ -7,31 +7,22 @@
 
 import UIKit
 
-@IBDesignable class PaddingLabel: UILabel {
+class PaddingLabel: UILabel {
     
-    @IBInspectable var topPadding: CGFloat = 0.0
-    @IBInspectable var leftPadding: CGFloat = 0.0
-    @IBInspectable var bottomPadding: CGFloat = 0.0
-    @IBInspectable var rightPadding: CGFloat = 0.0
-    
-    convenience init(padding: UIEdgeInsets) {
-        self.init()
-        self.topPadding = padding.top
-        self.leftPadding = padding.left
-        self.bottomPadding = padding.bottom
-        self.rightPadding = padding.right
-    }
+    var padding: UIEdgeInsets = .zero
     
     override func drawText(in rect: CGRect) {
-        let padding = UIEdgeInsets.init(top: topPadding, left: leftPadding, bottom: bottomPadding, right: rightPadding)
-        super.drawText(in: rect.inset(by: padding))
+        let insets = padding
+        super.drawText(in: rect.inset(by: insets))
     }
     
     override var intrinsicContentSize: CGSize {
-        var contentSize = super.intrinsicContentSize
-        contentSize.width += self.leftPadding + self.rightPadding
-        contentSize.height += self.topPadding + self.bottomPadding
-        return contentSize
+        let size = super.intrinsicContentSize
+        return CGSize(width: size.width + padding.left + padding.right, height: size.height + padding.top + padding.bottom)
+    }
+    
+    override var bounds: CGRect {
+        didSet { preferredMaxLayoutWidth = bounds.width - (padding.left + padding.right) }
     }
     
 }
