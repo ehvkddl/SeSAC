766391ed5e537eb54b4f8b203ea72a133e9c88b7
ehvkddl <ehvkddl@naver.com>
2023-08-21 00:43:28 +0900

    feat: fetchSearch 구현

---
+++ week5/Media/Media/Network/TmdbAPIManager.swift
@@ -0,0 +1,45 @@
+//
+//  TmdbAPIManager.swift
+//  Media
+//
+//  Created by do hee kim on 2023/08/21.
+//
+
+import Foundation
+import Alamofire
+
+class TmdbAPIManager {
+    static let shared = TmdbAPIManager()
+    private init() {}
+    
+    let headers: HTTPHeaders = [
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MmFhNTI3ZWZkYjIwZWI5OTlkY2VjMWJmYWE4NWM5NiIsInN1YiI6IjY0ZDEzNzBkZDhkMzI5MDExZTc0YmE0ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.b02Ozt3Zntfjrhbw1uoEN-VJtaSF9zPxTkAG9gBIV78"
+    ]
+    
+    let language = URLQueryItem(name: "language", value: "ko-KR")
+    let apiKey = URLQueryItem(name: "api_key", value: "\(APIKey.tmdbKey)")
+    
+    // MARK: - fetchSearch
+    func fetchSearch(title query: String, completionHandler: @escaping ([Movie]) -> Void) {
+        let movieTitle = URLQueryItem(name: "query", value: query)
+        
+        var components = URLComponents(string: EndPoint.search.requestURL)
+        components?.queryItems = [language, apiKey, movieTitle]
+        
+        guard let url = components?.string else { return }
+        
+        AF.request(url, method: .get).validate()
+            .responseDecodable(of: SearchData.self) { response in
+                switch response.result {
+                case .success(let value):
+                    
+                    completionHandler(value.results)
+                    
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
+    
+}
