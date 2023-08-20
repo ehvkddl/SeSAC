f4565bc033ae27b69a1694a885cb0be55ac9d1b2
ehvkddl <ehvkddl@naver.com>
2023-08-21 00:46:59 +0900

    rename: cell 재활용하기 위해서 더 포괄적인 이름으로 변경

--- week5/Media/Media/Cells/MovieTableViewCell.swift
+++ week5/Media/Media/Cells/MovieTableViewCell.swift
@@ -1,13 +1,13 @@
 //
-//  SimilarTableViewCell.swift
+//  MovieTableViewCell.swift
 //  Media
 //
-//  Created by do hee kim on 2023/08/20.
+//  Created by do hee kim on 2023/08/21.
 //
 
 import UIKit
 
-class SimilarTableViewCell: UITableViewCell {
+class MovieTableViewCell: UITableViewCell {
 
     @IBOutlet var posterImageView: UIImageView!
     
