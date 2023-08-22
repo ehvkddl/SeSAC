bbba000c810b1c9cd85845694fb1da14f7f87fed
ehvkddl <ehvkddl@naver.com>
2023-08-23 05:00:06 +0900

    feat: Padding이 있는 UILabel

---
+++ week6/CodeBase/CodeBase/PaddingLabel.swift
@@ -0,0 +1,31 @@
+//
+//  PaddingLabel.swift
+//  CodeBase
+//
+//  Created by do hee kim on 2023/08/23.
+//
+
+import UIKit
+
+class PaddingLabel: UILabel {
+    
+    private var padding = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+
+    convenience init(padding: UIEdgeInsets) {
+        self.init()
+        self.padding = padding
+    }
+
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: padding))
+    }
+
+    override var intrinsicContentSize: CGSize {
+        var contentSize = super.intrinsicContentSize
+        contentSize.height += padding.top + padding.bottom
+        contentSize.width += padding.left + padding.right
+
+        return contentSize
+    }
+    
+}
