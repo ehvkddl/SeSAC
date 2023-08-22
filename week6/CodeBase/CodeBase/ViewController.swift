c6213b10a024bb53167ab6766d47bc77264068f3
ehvkddl <ehvkddl@naver.com>
2023-08-22 17:10:47 +0900

    feat: SnapKit 사용하여 button 생성

--- week6/CodeBase/CodeBase/ViewController.swift
+++ week6/CodeBase/CodeBase/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    let example1Button = {
+        let button = UIButton()
+        
+        button.setTitle("example 1", for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = .systemOrange
+        button.layer.cornerRadius = 10
+        
+        return button
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureUI()
+    }
+    
     }
 
 
 }
 
+extension ViewController {
+    
+    func configureUI() {
+        [example1Button, example2Button, example3Button].forEach { view.addSubview($0) }
+        
+        
+        // MARK: SnapKit
+        example1Button.snp.makeConstraints { make in
+            make.bottom.equalTo(example2Button.snp.top).offset(-80)
+            make.centerX.equalTo(view)
+            make.width.equalTo(200)
+            make.height.equalTo(80)
+        }
+
+    }
+    
+}
