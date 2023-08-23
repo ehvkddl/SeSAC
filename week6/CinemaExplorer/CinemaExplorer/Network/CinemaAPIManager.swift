7dd860ac3d39e51c4f57928faf5e0f3eb741f802
ehvkddl <ehvkddl@naver.com>
2023-08-24 00:53:38 +0900

    add: CinemaAPImanager 생성

---
+++ week6/CinemaExplorer/CinemaExplorer/Network/CinemaAPIManager.swift
@@ -0,0 +1,15 @@
+//
+//  CinemaAPIManager.swift
+//  CinemaExplorer
+//
+//  Created by do hee kim on 2023/08/24.
+//
+
+import Foundation
+
+class CinemaAPImanager {
+    
+    static let shared = CinemaAPImanager()
+    private init() {}
+    
+}
