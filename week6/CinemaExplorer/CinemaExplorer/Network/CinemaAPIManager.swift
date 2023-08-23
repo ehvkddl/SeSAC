7e9c1363be6c4bdb65996401187da55127cfb21b
ehvkddl <ehvkddl@naver.com>
2023-08-24 03:17:45 +0900

    feat: featLocations 메서드 구현
    - Kakao API 사용

--- week6/CinemaExplorer/CinemaExplorer/Network/CinemaAPIManager.swift
+++ week6/CinemaExplorer/CinemaExplorer/Network/CinemaAPIManager.swift
@@ -6,10 +6,36 @@
 //
 
 import Foundation
+import CoreLocation
+import Alamofire
 
 class CinemaAPImanager {
     
     static let shared = CinemaAPImanager()
     private init() {}
+    
+    let headers: HTTPHeaders = [
+        "Authorization": "KakaoAK \(APIKey.kakaoKey)"
+    ]
+    
+    func fetchLocations(around location: CLLocationCoordinate2D) {
+        let parameters: Parameters = [
+            "x": location.longitude,
+            "y": location.latitude,
+            "radius": 2000
+        ]
+        
+        let url = Endpoint.Search.Keyword.cinema.requestURL
+        
+        AF.request(url, method: .get, parameters: parameters, headers: headers).validate()
+            .responseDecodable(of: LocationData.self) { response in
+                switch response.result {
+                case .success(let value):
+                    print(value)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
     
 }
