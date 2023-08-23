150ed399e7a7cd8de018ad1a6045b609255b983b
ehvkddl <ehvkddl@naver.com>
2023-08-23 23:10:28 +0900

    init: CinemaExplorer 생성

---
+++ week6/CinemaExplorer/CinemaExplorer/ExplorerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CinemaExplorer
+//
+//  Created by do hee kim on 2023/08/23.
+//
+
+import UIKit
+
+class ExplorerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
