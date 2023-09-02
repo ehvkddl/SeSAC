9f9b0ed5bae6a3dc1a8712eff205138d8b5339ff
ehvkddl <ehvkddl@naver.com>
2023-09-03 03:20:22 +0900

    feat: PHPickerViewController 사용하여 갤러리에서 사진 가져오기

--- 014_PhotoGram/PhotoGram/Scenes/Photo/PhotoViewController.swift
+++ 014_PhotoGram/PhotoGram/Scenes/Photo/PhotoViewController.swift
@@ -6,9 +6,19 @@
 //
 
 import UIKit
+import PhotosUI
 
 class PhotoViewController: BaseViewController {
 
+    let phpicker = {
+        var configuration = PHPickerConfiguration()
+        configuration.selectionLimit = 1
+        configuration.filter = .images
+        
+        let picker = PHPickerViewController(configuration: configuration)
+        return picker
+    }()
+    
     let mainView = PhotoView()
     
     override func loadView() {
@@ -23,7 +33,9 @@ class PhotoViewController: BaseViewController {
     func photoChangeButtonClicked() {
         let alert = UIAlertController(title: "사진 가져올 방법을 선택해주세요", message: nil, preferredStyle: .actionSheet)
         
-        let gallery = UIAlertAction(title: "갤러리에서 가져오기", style: .default)
+        let gallery = UIAlertAction(title: "갤러리에서 가져오기", style: .default) { action in
+            self.getPhotoFromGallery()
+        }
         let webSearch = UIAlertAction(title: "웹에서 검색하기", style: .default)
         let cancel = UIAlertAction(title: "취소", style: .cancel)
         
@@ -33,9 +45,35 @@ class PhotoViewController: BaseViewController {
         
         present(alert, animated: true)
     }
+    
+    func getPhotoFromGallery() {
+        present(phpicker, animated: true)
+    }
+    
     override func configureView() {
         super.configureView()
         
         mainView.photoChangeButton.addTarget(self, action: #selector(photoChangeButtonClicked), for: .touchUpInside)
+        
+        phpicker.delegate = self
     }
+    
+}
+
+extension PhotoViewController: PHPickerViewControllerDelegate {
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        guard let result = results.first else { return }
+        
+        result.itemProvider.loadObject(ofClass: UIImage.self) { object, error in
+            if let image = object as? UIImage {
+                DispatchQueue.main.async {
+                    self.mainView.photoImage.image = image
+                }
+            }
+        }
+        
+        picker.dismiss(animated: true)
+    }
+    
 }
