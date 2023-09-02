e10c712c94c5782763084ed88970abf03d3fc39a
ehvkddl <ehvkddl@naver.com>
2023-09-03 06:00:01 +0900

    add: unsplash에서 가져온 사진 정보 저장할 구조체 생성

---
+++ 014_PhotoGram/PhotoGram/Models/Photo.swift
@@ -0,0 +1,24 @@
+//
+//  Photo.swift
+//  PhotoGram
+//
+//  Created by do hee kim on 2023/09/03.
+//
+
+import Foundation
+
+struct Photo: Codable {
+    let total: Int
+    let total_pages: Int
+    let results: [PhotoResult]
+}
+
+struct PhotoResult: Codable {
+    let id: String
+    let urls: PhotoURL
+}
+
+struct PhotoURL: Codable {
+    let full: String
+    let thumb: String
+}
