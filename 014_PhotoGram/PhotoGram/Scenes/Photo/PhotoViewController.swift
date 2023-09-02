d6d1d71b1d70e16b8095cee04e3f488abcded1d2
ehvkddl <ehvkddl@naver.com>
2023-09-03 02:38:03 +0900

    feat: 사진 가져오기 버튼에 action sheet 추가

--- 014_PhotoGram/PhotoGram/Scenes/Photo/PhotoViewController.swift
+++ 014_PhotoGram/PhotoGram/Scenes/Photo/PhotoViewController.swift
@@ -19,4 +19,23 @@ class PhotoViewController: BaseViewController {
         super.viewDidLoad()
     }
     
+    @objc
+    func photoChangeButtonClicked() {
+        let alert = UIAlertController(title: "사진 가져올 방법을 선택해주세요", message: nil, preferredStyle: .actionSheet)
+        
+        let gallery = UIAlertAction(title: "갤러리에서 가져오기", style: .default)
+        let webSearch = UIAlertAction(title: "웹에서 검색하기", style: .default)
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        
+        alert.addAction(gallery)
+        alert.addAction(webSearch)
+        alert.addAction(cancel)
+        
+        present(alert, animated: true)
+    }
+    override func configureView() {
+        super.configureView()
+        
+        mainView.photoChangeButton.addTarget(self, action: #selector(photoChangeButtonClicked), for: .touchUpInside)
+    }
 }
