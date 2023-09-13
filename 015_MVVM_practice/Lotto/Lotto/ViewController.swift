9aa7e896fc155a4629e4252322d7fea74b4a9f0e
ehvkddl <ehvkddl@naver.com>
2023-09-13 23:36:21 +0900

    init: Lotto 생성

---
+++ 015_MVVM_practice/Lotto/Lotto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lotto
+//
+//  Created by do hee kim on 2023/09/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
