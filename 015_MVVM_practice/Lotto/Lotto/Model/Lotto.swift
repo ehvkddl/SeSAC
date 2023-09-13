4b7e7c7900f04e626c79e5810359c9265c2d6a87
ehvkddl <ehvkddl@naver.com>
2023-09-14 00:01:10 +0900

    feat: 로또 정보 가져오기

---
+++ 015_MVVM_practice/Lotto/Lotto/Model/Lotto.swift
@@ -0,0 +1,25 @@
+//
+//  Lotto.swift
+//  Lotto
+//
+//  Created by do hee kim on 2023/09/13.
+//
+
+import Foundation
+
+struct Lotto: Codable {
+    var totSellamnt: Int
+    var returnValue: String
+    var drwNoDate: String
+    var drwNo: Int
+    var drwtNo1: Int
+    var drwtNo2: Int
+    var drwtNo3: Int
+    var drwtNo4: Int
+    var drwtNo5: Int
+    var drwtNo6: Int
+    var bnusNo: Int
+    var firstWinamnt: Int
+    var firstPrzwnerCo: Int
+    var firstAccumamnt: Int
+}
---
+++ 015_MVVM_practice/Lotto/Lotto/Network/LottoAPIManager.swift
@@ -0,0 +1,56 @@
+//
+//  LottoAPIManager.swift
+//  Lotto
+//
+//  Created by do hee kim on 2023/09/13.
+//
+
+import Foundation
+
+class LottoAPIManager {
+    
+    func fetchLotto(round drwNo: Int, completionHandler: @escaping (Lotto) -> Void) {
+        guard let url = URL(string: "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=\(drwNo)") else {
+            print("URL 변환 실패")
+            return
+        }
+        
+        fetchData(from: url) { (lotto: Lotto) in
+            completionHandler(lotto)
+        }
+
+    }
+    
+    private func fetchData<T: Codable>(from url: URL, completionHandler: @escaping (T) -> Void) {
+        
+        let urlRequest = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            if let error {
+                print("request를 실패했습니다.", error)
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, (200...500).contains(response.statusCode) else {
+                print("status code가 200~500을 벗어났습니다.")
+                return
+            }
+            
+            guard let data = data else {
+                print("data가 비어있습니다.")
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(T.self, from: data)
+                
+                completionHandler(result)
+                
+            } catch {
+                print("decoding을 실패했습니다.")
+            }
+        }.resume()
+        
+    }
+    
+}
