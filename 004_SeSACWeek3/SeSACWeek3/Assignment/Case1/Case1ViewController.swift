3faaeab1da24cc30cf95d8ef24ffebd2f19ee584
ehvkddl <ehvkddl@naver.com>
2023-09-17 03:58:15 +0900

    feat: Case1 Modern CollectionView
    - 그런데 내용없음을 곁들인,,

--- 004_SeSACWeek3/SeSACWeek3/Assignment/Case1/Case1ViewController.swift
+++ 004_SeSACWeek3/SeSACWeek3/Assignment/Case1/Case1ViewController.swift
@@ -6,22 +6,113 @@
 //
 
 import UIKit
+import SnapKit
 
 class Case1ViewController: UIViewController {
+    
+    enum Section: Int, CaseIterable {
+        case mode
+        case share
+        case state
+    }
+    
+    enum SetType {
+        case on
+        case off
+        case initial
+    }
+    
+    struct Item: Hashable {
+        let id = UUID().uuidString
+        let image: UIImage?
+        let imageColor: UIColor?
+        let title: String
+        let isSet: SetType?
+    }
+    
+    let modeList = [
+        Item(image: UIImage(systemName: "moon.fill"), imageColor: .systemIndigo, title: "방해금지 모드", isSet: .on),
+        Item(image: UIImage(systemName: "bed.double.fill"), imageColor: .systemMint, title: "수면", isSet: .off),
+        Item(image: UIImage(systemName: "person.fill"), imageColor: .systemPurple, title: "개인 시간", isSet: .initial),
+        Item(image: UIImage(systemName: "lanyardcard.fill"), imageColor: .systemTeal, title: "업무", isSet: .initial)
+    ]
+    
+    let shareList = [
+        Item(image: nil, imageColor: nil, title: "모든 기기에서 공유", isSet: nil)
+    ]
+    
+    let stateList = [
+        Item(image: nil, imageColor: nil, title: "집중 모드 상태", isSet: nil)
+    ]
+    
+    lazy var collectionView = {
+        let view = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        return view
+    }()
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "집중 모드"
+        
         configureView()
         setConstraints()
+        
+        configureDataSource()
     }
     
     func configureView() {
         view.backgroundColor = .systemBackground
+        
+        [collectionView].forEach { view.addSubview($0) }
     }
     
     func setConstraints() {
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+}
+
+extension Case1ViewController {
+    
+    func createLayout() -> UICollectionViewLayout{
+        let config = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        let layout = UICollectionViewCompositionalLayout.list(using: config)
+        return layout
+    }
+    
+    func configureDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Item> { cell, indexPath, itemIdentifier in
+            var contentConfiguration = UIListContentConfiguration.valueCell()
+            print("외않되?")
+            print(itemIdentifier.title)
+            contentConfiguration.text = "타이틀"
+//            contentConfiguration.image = itemIdentifier.image
+//            contentConfiguration.imageProperties.tintColor = itemIdentifier.imageColor
+//            contentConfiguration.text = itemIdentifier.title
+//            switch itemIdentifier.isSet {
+//            case .initial: contentConfiguration.secondaryText = "설정"
+//            case .on: contentConfiguration.secondaryText = "켬"
+//            case .off: contentConfiguration.secondaryText = ""
+//            default: break
+//            }
+        }
         
+        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            let cell = collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+            print(itemIdentifier.title)
+            return cell
+        })
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections(Section.allCases)
+        snapshot.appendItems(modeList, toSection: .mode)
+        snapshot.appendItems(shareList, toSection: .share)
+        snapshot.appendItems(stateList, toSection: .state)
+        dataSource.apply(snapshot)
     }
     
 }
