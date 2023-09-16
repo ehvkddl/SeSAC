f8f0e6bc7bad5099362875bdf93c4a03d3856daf
ehvkddl <ehvkddl@naver.com>
2023-09-16 21:07:15 +0900

    add: Case1ViewController 추가

---
+++ 004_SeSACWeek3/SeSACWeek3/Assignment/Case1/Case1ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  Case1ViewController.swift
+//  SeSACWeek3
+//
+//  Created by do hee kim on 2023/09/16.
+//
+
+import UIKit
+
+class Case1ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureView()
+        setConstraints()
+    }
+    
+    func configureView() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    func setConstraints() {
+        
+    }
+    
+}
