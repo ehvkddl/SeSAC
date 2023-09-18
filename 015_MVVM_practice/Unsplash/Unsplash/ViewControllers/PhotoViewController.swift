316893b7ba2aeb45fef44f0b5b13a74087bb6721
ehvkddl <ehvkddl@naver.com>
2023-09-19 00:34:02 +0900

    design: Random Photo 보여주기 위한 imageView와 button 추가

--- 015_MVVM_practice/Unsplash/Unsplash/ViewControllers/PhotoViewController.swift
+++ 015_MVVM_practice/Unsplash/Unsplash/ViewControllers/PhotoViewController.swift
@@ -17,13 +17,54 @@ class PhotoViewController: BaseViewController {
         return view
     }()
     
+    let photoImageView = {
+        let view = UIImageView()
+        return view
+    }()
+    
+    let refreshButton = {
+        let btn = UIButton()
+        var config = UIButton.Configuration.filled()
+        config.image = UIImage(systemName: "arrow.clockwise")
+        config.baseBackgroundColor = .black
+        config.baseForegroundColor = .white
+        btn.configuration = config
+        return btn
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func configureView() {
+        super.configureView()
+        
+        [photoImageView, refreshButton].forEach { view.addSubview($0) }
+        
+        refreshButton.addTarget(self, action: #selector(refreshButtonClicked), for: .touchUpInside)
+    }
+    
+    override func setConstraints() {
+        photoImageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.height.equalTo(view.safeAreaLayoutGuide).multipliedBy(0.6)
+        }
+        
+        refreshButton.snp.makeConstraints { make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.centerX.equalToSuperview()
+            make.size.equalTo(50)
+        }
     }
     
     override func setNavigationBar() {
         navigationItem.title = "Unsplash"
         navigationItem.searchController = searchController
+    }
+    
+    @objc func refreshButtonClicked() {
+        print("refreshButtonClicked")
     }
 
 }
