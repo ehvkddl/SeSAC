e8f143adc95f9cfc887a0fb4cc2ec09b7f334f9a
ehvkddl <ehvkddl@naver.com>
2023-11-21 02:59:26 +0900

    rename: 폴더 구조 변경

--- 018_Otdo/Otdo/Scene/Login/LoginViewController.swift
+++ 018_Otdo/Otdo/Scene/Login/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Otdo
 //
 //  Created by do hee kim on 2023/11/21.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
