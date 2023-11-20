f542f84785e55729a64bc26344f626a048878cff
ehvkddl <ehvkddl@naver.com>
2023-11-21 03:23:59 +0900

    feat: Constants, Color 추가

---
+++ 018_Otdo/Otdo/Utility/Color.swift
@@ -0,0 +1,16 @@
+//
+//  Color.swift
+//  Otdo
+//
+//  Created by do hee kim on 2023/11/21.
+//
+
+import UIKit
+
+extension Constants {
+    enum Color {
+        
+        static let accent: UIColor = .accent
+        
+    }
+}
---
+++ 018_Otdo/Otdo/Utility/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Otdo
+//
+//  Created by do hee kim on 2023/11/21.
+//
+
+import Foundation
+
+enum Constants {
+    
+    static let cornerRadius: CGFloat = 15
+    
+}
