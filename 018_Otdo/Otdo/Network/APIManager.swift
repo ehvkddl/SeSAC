02389f1c40fd9f6303434a3cedf452b8718f03bb
ehvkddl <ehvkddl@naver.com>
2023-12-04 23:59:03 +0900

    feat: APIManager 생성

---
+++ 018_Otdo/Otdo/Network/APIManager.swift
@@ -0,0 +1,15 @@
+//
+//  APIManager.swift
+//  Otdo
+//
+//  Created by do hee kim on 2023/12/04.
+//
+
+import Foundation
+
+class APIManager {
+    
+    static let shared = APIManager()
+    private init() { }
+    
+}
